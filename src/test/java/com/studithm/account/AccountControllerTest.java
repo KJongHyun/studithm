fbc96a698005dfac50239419d06b08c996dfd4bb
jonghyeonkim <whdgus8219@naver.com>
2020-06-15 22:52:39 +0900

    테스트 추가

--- src/test/java/com/studithm/account/AccountControllerTest.java
+++ src/test/java/com/studithm/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.studithm.account;
 
+import com.studithm.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,7 +66,9 @@ class AccountControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
 
-        assertTrue(accountRepository.existsByEmail("whdgus8219@naver.com"));
+        Account account = accountRepository.findByEmail("whdgus8219@naver.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "12345678");
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
