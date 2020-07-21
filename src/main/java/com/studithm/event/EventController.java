f8ecf2d87a40475cc8b11fa2dcc5845baebc9e2d
jonghyeonkim <whdgus8219@naver.com>
2020-07-21 18:56:24 +0900

    EventController 추가

---
+++ src/main/java/com/studithm/event/EventController.java
@@ -0,0 +1,28 @@
+package com.studithm.event;
+
+import com.studithm.account.CurrentAccount;
+import com.studithm.domain.Account;
+import com.studithm.domain.Study;
+import com.studithm.study.StudyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/study/{path}")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final StudyService studyService;
+
+    @GetMapping("/new-event")
+    public String newEventForm(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+
+        return "event/event";
+    }
+
+}
