e3dd30dca8d2ce37fc7407ecf3a73d3ac4c5e1ec
jonghyeonkim <whdgus8219@naver.com>
2020-07-21 21:13:22 +0900

    이벤트 추가 폼 개발

--- src/main/java/com/studithm/event/EventController.java
+++ src/main/java/com/studithm/event/EventController.java
@@ -3,6 +3,7 @@ package com.studithm.event;
 import com.studithm.account.CurrentAccount;
 import com.studithm.domain.Account;
 import com.studithm.domain.Study;
+import com.studithm.event.form.EventForm;
 import com.studithm.study.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -21,8 +22,11 @@ public class EventController {
     @GetMapping("/new-event")
     public String newEventForm(@CurrentAccount Account account, @PathVariable String path, Model model) {
         Study study = studyService.getStudyToUpdateStatus(account, path);
+        model.addAttribute(study);
+        model.addAttribute(account);
+        model.addAttribute(new EventForm());
 
-        return "event/event";
+        return "event/form";
     }
 
 }
